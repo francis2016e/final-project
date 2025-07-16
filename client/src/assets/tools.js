db0c0345355b4553ccf75409a67e535514bb7d47
francis2016e <emechidiebere1998@gmail.com>
2025-07-16 16:36:32 +0100

    added react, dependency and navbar

---
+++ client/src/assets/tools.js
@@ -0,0 +1,17 @@
+import electronics1 from "./electronics_1.jfif";
+// import electronics2 from "./electronics_2.jfif";
+// import electronics3 from "./electronics_3.jfif";
+// import electronics4 from "./electronics_4.jfif";
+// import electronics5 from "./electronics_5.jfif";
+// import bars_solid from "bars-solid(2).svg";
+// import bell_regular from "./bell_regular.svg";
+import user_solid from "./user_solid.svg"
+import cart_shopping from "./cart-shopping-solid.svg";
+import ZeeZade from "./ZeeZade_logo.jfif";
+
+export const tools = {
+  electronics1,
+  ZeeZade,
+  cart_shopping,
+  user_solid
+}
