18f4af1b6c125096232e1ce2c4467c69f6ee1c85
francis2016e <emechidiebere1998@gmail.com>
2025-07-19 18:09:46 +0100

    added main home screen banner

--- client/src/assets/tools.js
+++ client/src/assets/tools.js
@@ -8,10 +8,28 @@ import electronics1 from "./electronics_1.jfif";
 import user_solid from "./user_solid.svg"
 import cart_shopping from "./cart-shopping-solid.svg";
 import ZeeZade from "./ZeeZade_logo.jfif";
+import samsung2 from "./samsung2.jfif";
+import samsung from "./samsung.jfif";
+import main_bg from "./main_bg2.jfif";
+import itel_phone from "./itel_phone.jfif";
+import headset from "./headset.jfif";
+import blinder from "./blinder.jfif";
+import electronics_bg from "./electronic_bg.jfif";
+import arrow from "./arrow-right-solid.svg";
+
+
 
 export const tools = {
   electronics1,
   ZeeZade,
   cart_shopping,
-  user_solid
+  user_solid,
+  samsung2,
+  samsung,
+  main_bg,
+  itel_phone,
+  headset,
+  blinder,
+  electronics_bg,
+  arrow
 }
