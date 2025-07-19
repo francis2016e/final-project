3f6a10f420017daf2989e27144a51e87547b11de
francis2016e <emechidiebere1998@gmail.com>
2025-07-19 22:49:14 +0100

    added items on the home page

--- client/src/assets/tools.js
+++ client/src/assets/tools.js
@@ -1,10 +1,10 @@
 import electronics1 from "./electronics_1.jfif";
-// import electronics2 from "./electronics_2.jfif";
-// import electronics3 from "./electronics_3.jfif";
-// import electronics4 from "./electronics_4.jfif";
-// import electronics5 from "./electronics_5.jfif";
-// import bars_solid from "bars-solid(2).svg";
-// import bell_regular from "./bell_regular.svg";
+import electronics2 from "./electronics_2.jfif";
+import electronics3 from "./electronics_3.jfif";
+import electronics4 from "./electronics_4.jfif";
+import electronics5 from "./electronics_5.jfif";
+// import bars_solid from "./bars.svg";
+import bell_regular from "./bell-regular.svg";
 import user_solid from "./user_solid.svg"
 import cart_shopping from "./cart-shopping-solid.svg";
 import ZeeZade from "./ZeeZade_logo.jfif";
@@ -31,5 +31,94 @@ export const tools = {
   headset,
   blinder,
   electronics_bg,
-  arrow
+  arrow,
+  electronics1,
+  electronics2,
+  electronics3,
+  electronics4,
+  electronics5,
+  // bars_solid,
+  bell_regular
 }
+
+
+export const categories = [
+  {
+    text: "Phone",
+    path: "samsung",
+    Image: samsung2,
+    bgColor: "#FEF6DA"
+
+  },
+
+  {
+    text: "Phone",
+    path: "samsung",
+    Image: samsung,
+    bgColor: "#FEF6DA"
+
+  },
+
+  {
+    text: "Blender",
+    path: "grinder",
+    Image: blinder,
+    bgColor: "#FEE0E0"
+
+  },
+
+    {
+    text: "Phone",
+    path: "itel",
+    Image:  itel_phone,
+    bgColor: "#FEE0E0"
+
+  },
+
+     {
+    text: "Headset",
+    path: "itel",
+    Image:  headset,
+    bgColor: "#FEE0E0"
+
+  },
+{
+  text: "headset",
+    path: "itel",
+    Image:  electronics1,
+    bgColor: "#FEE0E0"
+
+  },
+
+    {
+    text: "Phone",
+    path: "samsung",
+    Image: electronics2,
+    bgColor: "#FEF6DA"
+
+  },
+
+    {
+    text: "Phone",
+    path: "samsung",
+    Image: electronics3,
+    bgColor: "#FEF6DA"
+
+  },
+
+    {
+    text: "Phone",
+    path: "samsung",
+    Image: electronics4,
+    bgColor: "#FEF6DA"
+
+  },
+
+    {
+    text: "Phone",
+    path: "samsung",
+    Image: electronics5,
+    bgColor: "#FEF6DA"
+
+  },
+]
--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -9,7 +9,7 @@ const MainBanner = () => {
 
 
       <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-10 md:pb-0 Px-4 md:pl-18 lg:pl-24'>
-        <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 '>Get the best electronic gadget that will give you the best experience</h1>
+        <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 '>Get the best electronic gadget that will give you the best experience!</h1>
       
 
 
